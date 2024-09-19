a48282d6244901d09545c24aa6f374aad746fa49
Drosos Drosinakis <drosinakis.drosos1@gmail.com>
2024-09-19 18:40:40 +0300

    add autocmd tailwindsort

--- lua/xaosmaker/autocmd.lua
+++ lua/xaosmaker/autocmd.lua
@@ -1,13 +1,15 @@
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 
-
-
-
-
-
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+vim.api.nvim_create_autocmd("BufWritePre", {
+	desc = "auto short the tailwind functions",
+	group = vim.api.nvim_create_augroup("tailwind-short", { clear = true }),
+	callback = function()
+		vim.cmd("TailwindSort")
+	end,
 })
