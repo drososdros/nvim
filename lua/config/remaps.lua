2690b336d48193226cc7ef2618f7ea4049aeb2e1
Drosos <drosinakis.drosos@gmail.com>
2025-05-05 18:58:11 +0300

    add resize window with ctrl + up | down

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -30,6 +30,8 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<C-up>", "<C-w><C-+>", { desc = "resize window height +" })
+vim.keymap.set("n", "<C-down>", "<C-w><C-->", { desc = "resize window height -" })
 
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = " show diagnostics for line" })
 vim.keymap.set("n", "\\", "<cmd>NvimTreeToggle<CR>")
